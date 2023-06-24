f141e891bc61fc953e3b4119351833f940e706f2
zhangzhibang <295854713@qq.com>
2023-06-25 01:40:16 +0800

    feat: 非父子组件通信-context

--- 6_create_react_app/2_learn_component/src/index.js
+++ 6_create_react_app/2_learn_component/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./8_组件作用域插槽/App";
+import App from "./9_非父子通信-Context/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
